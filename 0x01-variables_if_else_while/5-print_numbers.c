950791e98548ad52ae7f486f58397e328adb76ae
Bamikole <stephoj2306@gmail.com>
2022-10-17 11:26:09 +0100

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Return: give 0 value if success
+*/
+
+int main(void)
+
+{
+	int n = 0;
+
+	while
+
+		(n < 10)
+		{
+			printf("%d", n);
+			n++;
+		}
+	printf("\n");
+	return (0);
+}
+
