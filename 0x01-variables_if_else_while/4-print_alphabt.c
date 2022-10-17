8d887ca6fdd3be4c008ff3b144fd06d1ae4b667a
Bamikole <stephoj2306@gmail.com>
2022-10-17 06:01:05 +0100

    Task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Return: gives 0 if (success)
+ */
+
+int main(void)
+
+{
+	char a = 'a';
+
+	while
+
+		(a <= 'z')
+		{
+			if (a != 'e' && a != 'q')
+				putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
+
+
+
