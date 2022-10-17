0ec5596b1a492cc2124f9f1bdb4d942a49d9f486
Bamikole <stephoj2306@gmail.com>
2022-10-17 14:37:00 +0100

    Task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Return: gives a value 0 if success
+ */
+
+int main(void)
+{
+	int n = 0;
+	char a = 'a';
+
+	while
+
+		(n < 10)
+		{
+			putchar((n % 10) + '0');
+			n++;
+		}
+
+	while
+
+		(a <= 'f')
+		{
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
+
