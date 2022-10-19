05cae81a1824ce0a246e74920a1a0dc8970b5dd8
Bamikole <stephoj2306@gmail.com>
2022-10-19 15:22:41 +0100

    Task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+
+/**
+ * main -  prints _putchat and a new line
+ * Return: Gives 0 value if (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
