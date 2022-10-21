56011ef78ff1a31a5f402d5ae92f3ad050a6e23f
Bamikole <stephoj2306@gmail.com>
2022-10-21 21:54:08 +0100

    Task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number from 0 to 9
+ * Return: gives o vaule ifsuccess
+ */
+
+void print_numbers(void)
+
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		_putchar(a++ + '0');
+	}
+	 _putchar('\n');
+
