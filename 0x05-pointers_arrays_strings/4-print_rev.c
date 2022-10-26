c1fb4e434450b79030f93c4500a5082688437e08
Bamikole <stephoj2306@gmail.com>
2022-10-26 04:05:17 +0100

    Task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,4 @@
-iinclude "main.h"
+include "main.h"
 
 /**
  * print_rev -  prints string is reverse order
@@ -10,13 +10,8 @@ void print_rev(char *s)
 	int i = 0;
 
 	while (s[i] != '\0')
-	{
 		i++;
-	}
 	for (i = i - 1; i >= 0; i--)
-	{
 		_putchar(s[i]);
-	}
 	_putchar('\n');
 }
-
