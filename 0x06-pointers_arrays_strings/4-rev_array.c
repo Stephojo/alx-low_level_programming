79a8fb78b3024095ce5271d63ef9fc7df99a1dec
Bamikole <stephoj2306@gmail.com>
2022-10-26 18:36:59 +0100

    Task4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses array of integers
+ * @a: parameter1
+ * @n: parameter2
+ * Return: reversed array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+}
+
