fe4dfcc394a21b7c7d341660b40941dfaedce754
Bamikole <stephoj2306@gmail.com>
2022-10-26 05:02:54 +0100

    Task 6 updated

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,12 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (; str[i] != '\0'; i + 2)
+	for (; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
 	}
 	_putchar('\n');
 }
