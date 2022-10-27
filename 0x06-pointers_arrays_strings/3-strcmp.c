bf5802bab82b98fd0e8a9ffe8b224f10499f7154
Bamikole <stephoj2306@gmail.com>
2022-10-27 20:45:46 +0100

    Task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,6 +9,14 @@ iinclude "main.h"
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != *s2 && *si == *s2)
-	return (*s1 - *s2);
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
 }
